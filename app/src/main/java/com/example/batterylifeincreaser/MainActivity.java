8ca5768821f987bab9d01a23207e505b01569adb
Ankur Maurya <ankurmaurya421@gmail.com>
2022-03-04 14:50:49 +0530

    made seekbar functionable

---
+++ app/src/main/java/com/example/batterylifeincreaser/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.batterylifeincreaser;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar lowerslider=findViewById(R.id.lowerslider);
+        lowerslider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView lower=findViewById(R.id.lowerlimit);
+                lower.setText(Integer.toString(i)+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        SeekBar upperslider=findViewById(R.id.upperslider);
+        upperslider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                TextView lower=findViewById(R.id.upperlimit);
+                lower.setText(Integer.toString(i)+"%");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
